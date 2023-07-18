067b32f67f7683b73de43222a69f9bf85e9f4392
galdevops <galdevops@gmail.com>
2023-07-18 13:49:29 +0300

    jenans12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hi DevSecOps12 Red-Project Implementation!</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
