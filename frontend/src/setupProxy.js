d70c513d6ae216e303393b42a95220eb732b8aba
galdevops <galdevops@gmail.com>
2023-07-18 14:25:07 +0300

    jenans14

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://backend:3001',
-      // target: `http://${process.env.REACT_APP_BACKEND}:3001`,
+      // target: 'http://backend:3001',
+      target: `http://${process.env.REACT_APP_BACKEND}:3001`,
       changeOrigin: true,
     })
   );
